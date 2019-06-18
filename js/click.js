9cec8a91df6b7f1ca58c68e335121d9a7e671d30
John Loomis <johnaloomis@outlook.com>
2019-06-18 13:46:14 -0400

    intial commit

---
+++ js/click.js
@@ -0,0 +1,13 @@
+
+AFRAME.registerComponent('marker', {
+    init: function () {
+        const redirect = document.getElementById("Logo");
+
+
+
+        redirect.addEventListener('click', function (evt) {
+            window.location.replace("https://covalentreality.co/");
+        })
+
+    }
+});
